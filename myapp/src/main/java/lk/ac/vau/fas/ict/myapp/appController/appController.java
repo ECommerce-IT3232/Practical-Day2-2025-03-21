fc8548ed0d89d07c803b0e4388b16a76b8e16c1d
Kasun Rajapaksha <139611912+Kasunchira@users.noreply.github.com>
2025-03-22 14:48:53 +0000

    cname class created

--- myapp/src/main/java/lk/ac/vau/fas/ict/myapp/appController/appController.java
+++ myapp/src/main/java/lk/ac/vau/fas/ict/myapp/appController/appController.java
@@ -1,5 +1,16 @@
-package lk.ac.vau.fas.ict.myapp.appController;
+package controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@GetMapping("/app")
 
 public class appController {
-
+	@GetMapping("/name")
+	public String myName() {
+	    return "Hello SpringBoot.";
+	}
 }
